f138d2122d4c6c91df80f63a7babae4bc8d0501c
Jon <xiajia@vip.qq.com>
2019-11-05 18:34:37 +0800

    增加跨域处理

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.all('/*', (req, res) => {
+app.all('*', (req, res, next) => {
+  // 0. 对跨域处理
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type")
+  res.header("Access-Control-Allow-Methods","PATCH,PUT,POST,GET,DELETE,OPTIONS")
+  res.header("Content-Type", "application/json;charset=utf-8")
+  next()
   // 1. 识别路径
   const path = req.path
   // 2. 找到data数据
